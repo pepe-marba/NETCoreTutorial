ccd5a87223c84c5a14a2579ae5b24b9ac06c6f9d
Jose Barcenas <jose.martinezb@outlook.com>
2017-05-11 13:04:57 -0700

    Javascript module completed

---
+++ src/NetCoreTutorial/wwwroot/js/site.js
@@ -0,0 +1,44 @@
+﻿//site.js
+(function () {
+
+    //var ele = $("#username");// document.getElementById("username");
+    //ele.text("Pp Mtz2");//ele.innerHTML = "Pp Mtz";
+
+    //var main = $("#main");//document.getElementById("main");
+    //main.on("mouseenter", function () {
+    //    main.style= "background-color: #888;";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style= "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+    //var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    //$("#sidebarToogle").on("click", function () {
+    //    $sidebarAndWrapper.toggleClass("hide-sidebar");
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show SideBar");
+        }
+        else {
+            $(this).text("hide Side bar");
+        }
+    });
+    
+})();
+
+
+
+
