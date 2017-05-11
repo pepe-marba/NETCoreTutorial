b66b01c4480ca26fc90d8536fbe31d0e06f10b77
Jose Barcenas <jose.martinezb@outlook.com>
2017-05-11 15:11:35 -0700

    MVC6 module finished

---
+++ src/NETCoreTutorial/Controllers/Web/AppController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NETCoreTutorial.Services;
+using NETCoreTutorial.ViewModels;
+
+namespace NETCoreTutorial.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                //First parameter is to define whether the error will be assigned to a field 
+                // Or empty if is for all the model
+                ModelState.AddModelError("Email", "We Don't support AOL addresses");
+            }
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From The World", model.Message);
+
+                ViewBag.UserMessage = " Message sent";
+                ModelState.Clear();
+            }
+
+            return View();
+
+        }
+
+        public IActionResult About()
+        {
+
+            return View();
+        }
+    }
+}
