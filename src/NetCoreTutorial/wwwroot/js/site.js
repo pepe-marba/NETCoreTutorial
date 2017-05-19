05e7a2b41fdec4587984a6378b13fd8b72aabb5b
Jose Barcenas <jose.martinezb@outlook.com>
2017-05-19 09:42:29 -0700

    Bootstrap module finished

--- src/NetCoreTutorial/wwwroot/js/site.js
+++ src/NetCoreTutorial/wwwroot/js/site.js
@@ -26,14 +26,18 @@
     //});
 
     var $sidebarAndWrapper = $("#sidebar, #wrapper");
+    var $icon = $("#sidebarToggle i.fa");
+
 
     $("#sidebarToggle").on("click", function () {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show SideBar");
+            $icon.removeClass("fa fa-angle-left");
+            $icon.addClass("fa fa-angle-right");
         }
         else {
-            $(this).text("hide Side bar");
+            $icon.removeClass("fa fa-angle-right");
+            $icon.addClass("fa fa-angle-left");
         }
     });
     
